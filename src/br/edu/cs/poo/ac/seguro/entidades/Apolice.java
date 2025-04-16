3d98eb945982f0ee74c2d509c437ebbf84a16953
vinicius <vbs4@cesar.school>
2025-04-16 19:26:01 -0300

    classe apolice

---
+++ src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -0,0 +1,17 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Apolice {
+    private Veiculo veiculo;
+    private BigDecimal valorFranquia;
+    private BigDecimal valorPremio;
+    private BigDecimal valorMaximoSegurado;
+}
