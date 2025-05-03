f6a2a20c29a423cd3569866df97c1f16504eb099
Caio Melo <camm@cesar.school>
2025-05-03 18:14:53 -0300

    Implementação VeiculoDAO e alteração em Veiculo

--- src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -3,11 +3,12 @@ package br.edu.cs.poo.ac.seguro.entidades;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class Veiculo {
+public class Veiculo implements Serializable {
     private String placa;
     private int ano;
     private SeguradoEmpresa proprietarioEmpresa;
