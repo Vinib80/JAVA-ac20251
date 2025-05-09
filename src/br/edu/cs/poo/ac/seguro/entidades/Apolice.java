fd77ce5a5d8a6816a809497ab002a8029893d499
vinicius <vbs4@cesar.school>
2025-05-09 07:23:32 -0300

    ajustei a posicao do atributo numero

--- src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
+++ src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -6,14 +6,16 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @AllArgsConstructor
 public class Apolice implements Serializable {
+    private String numero;
     private Veiculo veiculo;
     private BigDecimal valorFranquia;
     private BigDecimal valorPremio;
     private BigDecimal valorMaximoSegurado;
-    private String numero;
+    private LocalDate dataInicioVigencia;
 }
