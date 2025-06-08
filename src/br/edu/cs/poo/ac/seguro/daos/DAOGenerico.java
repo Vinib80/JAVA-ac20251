7b4be905d16e26f40893c47b414187cd9e8470d8
Caio Melo <camm@cesar.school>
2025-06-08 16:42:11 -0300

    DAOGenerico mudanças(erradas)

--- src/br/edu/cs/poo/ac/seguro/daos/DAOGenerico.java
+++ src/br/edu/cs/poo/ac/seguro/daos/DAOGenerico.java
@@ -1,10 +1,32 @@
 package br.edu.cs.poo.ac.seguro.daos;
 
+import br.edu.cs.poo.ac.seguro.entidades.Registro;
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import java.io.Serializable;
 
-public abstract class DAOGenerico {
-    private CadastroObjetos cadastro;
+public abstract class DAOGenerico<T extends Registro> {
 
-    public abstract Class getClasseEntidade();
+    private CadastroObjetos<T> cadastro;
 
+    public DAOGenerico() {
+        cadastro = new CadastroObjetos<>(getClasseEntidade());
+    }
+
+    protected abstract Class<T> getClasseEntidade();
+
+    public void incluir(T obj) {
+        cadastro.incluir(obj.getIdUnico(), obj);
+    }
+
+    public void alterar(T obj) {
+        cadastro.alterar(obj.getIdUnico(), obj);
+    }
+
+    public T buscar(String idUnico) {
+        return cadastro.buscar(idUnico);
+    }
+
+    public Registro[] buscarTodos() {
+        return cadastro.buscarTodos();
+    }
 }
